27d249a4d1a37549ac1428c746e3dec082aba5aa
Ingvar Friðbjörnsson <ingvarfr@gmail.com>
2018-08-24 13:40:14 +0200

    Added menu functionality

--- VideoMenuApp/Program.cs
+++ VideoMenuApp/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace VideoMenuApp
 {
     class Program
     {
         static int sel = 0;
+        static int id = 1;
+        private static List<Video> Videos = new List<Video>();
 
         static void Main(string[] args)
         {
@@ -13,8 +16,6 @@ namespace VideoMenuApp
                 DoMenu();
                 DoSelection(sel);
             }
-
-
 
         }
 
@@ -80,38 +81,136 @@ namespace VideoMenuApp
 
             void SearchVideosByName()
             {
-                Console.WriteLine("You search Videos by name");
+                Console.WriteLine("Enter your searchterm:");
+                var searchTerm = Console.ReadLine().ToLower();
+                var searchList = new List<Video>();
+                foreach (var video in Videos)
+                {
+                    if(video.Name.ToLower().Contains(searchTerm))
+                    {
+                        searchList.Add(video);
+                    }
+                }
+                if (searchList.Count == 0)
+                {
+                    Console.WriteLine("There were no matching videos found");
+                }
+                else
+                {
+                    ListVideos(searchList);
+                }
                 Console.ReadLine();
             }
 
             void SearchVideosByGenre()
             {
-                Console.WriteLine("You search Videos by genre");
+                Console.WriteLine("Enter your searchterm:");
+                var searchTerm = Console.ReadLine().ToLower();
+                var searchList = new List<Video>();
+                foreach (var video in Videos)
+                {
+                    if (video.Genre.ToLower().Contains(searchTerm))
+                    {
+                        searchList.Add(video);
+                    }
+                }
+                if (searchList.Count == 0)
+                {
+                    Console.WriteLine("There were no matching videos found");
+                }
+                else
+                {
+                    Console.WriteLine("These videos were found");
+                    Console.WriteLine();
+                    ListVideos(searchList);
+                }
                 Console.ReadLine();
             }
         }
 
         private static void DeleteVideo()
         {
-            Console.WriteLine("You delete a video");
+            Console.WriteLine("Which video do you want to delete?");
+            var foundVideo = FindVideoById();
+            if(foundVideo != null)
+            {
+                Videos.Remove(foundVideo);
+                Console.WriteLine("The video was removed");
+            }
             Console.ReadLine();
         }
 
         private static void UpdateVideo()
         {
-            Console.WriteLine("You update a video");
+            Console.WriteLine("Which video do you want to update?");
+            var foundVideo = FindVideoById();
+
+            if(foundVideo != null)
+            {
+                Console.WriteLine("New name:");
+                foundVideo.Name = Console.ReadLine();
+                Console.WriteLine("New genre:");
+                foundVideo.Genre = Console.ReadLine();
+                Console.WriteLine("The video has been updated");
+            }
             Console.ReadLine();
+        }
+
+        private static Video FindVideoById()
+        {
+            Console.WriteLine("Enter the video id");
+            int id;
+            Video foundVideo = null;
+            int.TryParse(Console.ReadLine(), out id);
+            foreach (var video in Videos)
+            {
+                if (video.Id.Equals(id))
+                {
+                    foundVideo = video;
+                }
+            }
+            if (foundVideo == null)
+            {
+                Console.WriteLine("There is no video with that id");
+                return null;
+            }
+            else
+            {
+                return foundVideo;
+            }
         }
 
         private static void ListAllVideos()
         {
-            Console.WriteLine("You list all videos");
+            ListVideos(Videos);
+        }
+
+        private static void ListVideos(List<Video> videos)
+        {
+            if (videos.Count == 0)
+            {
+                Console.WriteLine("There ar no videos!");
+            }
+            else
+            {
+                foreach (var video in videos)
+                {
+                    Console.WriteLine("Id: {0}, Name: {1}, Genre: {2}", video.Id, video.Name, video.Genre);
+                    Console.WriteLine("-----------------------------------------");
+                }
+            }
             Console.ReadLine();
         }
 
         private static void CreateVideo()
         {
-            Console.WriteLine("You create a new video");
+            Console.WriteLine("What is the name of your video?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is the genre of your video?");
+            var genre = Console.ReadLine();
+            var video = new Video() {Id = id++, Name = name, Genre = genre};
+            Videos.Add(video);
+            Console.WriteLine("The video has been added");
             Console.ReadLine();
         }
 
